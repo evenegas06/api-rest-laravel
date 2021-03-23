8bc6d4c3a99b469eccd0a8be3fef802a8c58d8c5
Eduardo Venegas Rivera <evenegas06@hotmail.com>
2021-03-23 01:01:39 -0600

    Api Rest - Curso, clase 16

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/usuario/pruebas', [UserController::class, 'pruebas']);
+Route::get('/category/pruebas', [CategoryController::class, 'pruebas']);
+Route::get('/post/pruebas', [PostController::class, 'pruebas']);
